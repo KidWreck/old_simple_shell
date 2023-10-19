0f57aede71665aef91608362aa2af787c5af0bf7
KidWreck <139178012+KidWreck@users.noreply.github.com>
2023-10-19 15:42:31 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -15,10 +15,11 @@ extern char **environ;
 
 void prompt(char **argv, char **envh, bool f);
 void _run(char **r, char **argv, char **envp);
-int _cmp(char *s1, char *s2, int h);
+int _strncmp(char *s1, char *s2, int h);
 char *_cpy(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int h);
 char *_strncat(char *dest, const char *src, size_t h);
+int _cmp(char *s1, char *s2)
 int _strlen(char *s);
 char *_cat(char *dest, char *src);
 void _exit(char *klam);
