16fa5fd943d6c555fa4a95e3683a08c0ce895344
KidWreck <139178012+KidWreck@users.noreply.github.com>
2023-10-19 15:40:22 +0300

    Update CMP.c

--- CMP.c
+++ CMP.c
@@ -1,18 +1,18 @@
 #include "shell.h"
 /**
- * _cmp - compare
- * @s1: first string
- * @s2: second string
- * @h: number of byte
+ * _strncmp - compare
+ * @s1: string
+ * @s2: string
+ * @h: number
  * Return: string
  */
 
-int _cmp(char *s1, char *s2, int h)
+int _strncmp(char *s1, char *s2, int h)
 {
 	if (!h)
 		return (0);
 	if (*s1 == *s2)
-		return (*s1 ? _cmp(s1 + 1, s2 + 1, h - 1) : 0);
+		return (*s1 ? _strncmp(s1 + 1, s2 + 1, h - 1) : 0);
 	if (*s1)
 		return (1);
 	if (*s2)
