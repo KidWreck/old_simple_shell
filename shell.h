39ec1627ec2cdd0276fb8dbce4ab52ec3196f280
Hassan <blaugrana888@hotmail.com>
2023-10-19 07:31:37 -0700

    hi

--- shell.h
+++ shell.h
@@ -6,10 +6,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <sys/type.h>
 #include <signal.h>
 #include <stdbool.h>
 #define MAX 10
+
+/**
+ * struct denum - contains vars
+ * @cnt: lines cnt
+ */
+typedef struct denum
+{
+	int cnt;
+} denum;
 
 extern char **environ;
 
@@ -19,12 +27,14 @@ int _strncmp(char *s1, char *s2, int h);
 char *_cpy(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int h);
 char *_strncat(char *dest, const char *src, size_t h);
-int _cmp(char *s1, char *s2)
+int _cmp(char *s1, char *s2);
 int _strlen(char *s);
 char *_cat(char *dest, char *src);
 void _exit(char *klam);
-char *_path(char **argv, char *klam)
+char *_path(char **argv, char *klam);
 void _sig(int n);
 int _len(int h);
 char *_itoa(int h);
-void geterr(denum *h, char **argv, char *klam)
+void geterr(denum *h, char **argv, char *klam);
+
+#endif
