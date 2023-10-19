c3a30d3c78b8c8f2503bd66359a42bd76e6d92df
KidWreck <139178012+KidWreck@users.noreply.github.com>
2023-10-19 17:25:46 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -22,20 +22,22 @@ typedef struct denum
 
 extern char **environ;
 
-void prompt(char **argv, char **envh, bool f);
-void _run(char **r, char **argv, char **envp);
-int _strncmp(char *s1, char *s2, int h);
 char *_cpy(char *dest, char *src);
-char *_strncpy(char *dest, char *src, int h);
-char *_strncat(char *dest, const char *src, size_t h);
-int _cmp(char *s1, char *s2);
-int _strlen(char *s);
 char *_cat(char *dest, char *src);
-void _exit(char *klam);
-char *_path(char **argv, char *klam);
-void _sig(int n);
 int _len(int h);
 char *_itoa(int h);
 void geterr(denum *h, char **argv, char *klam);
+int _cmp(char *s1, char *s2);
+void _exit(char *klam);
+void _sig(int n);
+void prompt(char **argv, char **envh, bool f);
+char *_path(char **argv, char *klam);
+void _run(char **r, char **argv, char **envp);
+int _strncmp(char *s1, char *s2, int h);
+char *_strncpy(char *dest, char *src, int h);
+char *_strncat(char *dest, const char *src, size_t h);
+int _strlen(char *s);
+char *tr(char *klam);
+
 
 #endif
